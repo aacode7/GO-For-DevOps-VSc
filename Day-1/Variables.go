d041c5dda99808f21d7af896116fa6247e873728
Shaik Asif Umeed <shaikasifumeed15@gmail.com>
2023-12-16 16:16:27 +0530

    first commit

---
+++ Day-1/Variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// func main(){
+// 	var x int = 10
+// 	fmt.Println(x)
+// }
+
+func main(){
+	x := 20
+	fmt.Println(x)
+}
---
+++ Day-1/handling-error.go
@@ -0,0 +1,11 @@
+// package main
+
+
+// func main(){
+// 	func sqrt(x float64) (float64, error){
+// 		if x < 0{
+// 			return 0, errors.New("Negative values")
+// 		}
+// 		return math.Sqrt(x),nil
+// 	}
+// }
